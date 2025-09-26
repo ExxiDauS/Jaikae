684469761e0a1e5aa41b17ed830c48df0de5980b
KwnxpN <tonnum.kawin@gmail.com>
2025-09-26 14:11:47 +0700

    feat: add static explore page

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Pet, PetImage
 
 # Create your views here.
+class PetsView(View):
+    def get(self, request):
+        gender = request.GET.get("gender")
+        print("gender:", gender)
+        return render(request, "pets/pets_explore.html")
